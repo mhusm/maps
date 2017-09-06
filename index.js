950b02380c00fa0509711735e158f54aa09d4fb0
Maria Husmann <maria.husmann@gmail.com>
2017-09-06 09:52:29 +0200

    added home route

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var path = require('path');
 
 app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, "public/map.html"))
+  })
+
 
 http.listen(process.env.PORT || 8080, function(){
    // console.log('listening on *:8080');
